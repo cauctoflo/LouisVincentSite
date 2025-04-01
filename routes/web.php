4d16d25d61baa6f2af2c4aa2f01a90571b7eac56
Cauctoflo <florentin.devs@gmail.com>
2025-04-01 14:35:42 +0200

    install

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+Route::get('/test', function () {
+    return 'https://prod.liveshare.vsengsaas.visualstudio.com/join?9E034DAD63B48EB8B59F24F7634F9046988D';
+})->name('test');
